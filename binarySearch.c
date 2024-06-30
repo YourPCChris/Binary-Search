d060fd6cb9e5d42ad1f8a235cd26b7e14b0ff61c
Christopher Bennett <chrisjwbennett@gmail.com>
2024-06-30 21:28:27 +0100

    updating file

--- binarySearch.c
+++ binarySearch.c
@@ -17,18 +17,36 @@ void main()
 	//printf("We Ball\n");
 	printf("***Bianry Search***\n");
 
-	int arr[10];
-	int end = sizeof(arr / arr[0]);
-	int mid = end/2;
-	arr[10] = {0,1,2,3,4,5,6,7,8,9};
+	int keepGoing = 1;
+	int value;
 	int start = 0;
+	int end = 10;
+	int mid = (start+end)/2;
+	int arr[10] = {0,1,2,18,25,30,40,45,67,78};
+
+	printf("Enter the number to find its index: ");
+	scanf("%i", &value);
+	printf("You have chosen %i\n", value);
 
 
 	while (keepGoing == 1)
 	{
-		if (arr[mid] == value){printf(" value found at index %i", mid);}
-		else if (arr[mid] != value)
-		{
-			if (arr[mid] > end){
+		mid = (start + end)/2;
+
+		if (arr[mid] == value){
+			printf("Value found at index %i\n", mid);
+			keepGoing = 0;
+		}
+		if (start > end){
+			printf("Value is not in array\n");
+			keepGoing = 0;
+		}
+		if (arr[mid] < value){ //value is to the right
+			start = mid+1;
+		}
+		else if(arr[mid] > value){ //Value is to the left
+			end = mid-1;
+		}
+	}
 
 }
