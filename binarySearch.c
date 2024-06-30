4ad2e5a4b6054bb6490b35f07c95a1e89a404ab5
Christopher Bennett <chrisjwbennett@gmail.com>
2024-06-30 19:31:24 +0100

    updating

---
+++ binarySearch.c
@@ -0,0 +1,34 @@
+/*
+Christopher Bennett
+BianrySearch Alogrithm
+28/07/2024
+*/
+
+
+
+
+#include <stdio.h>
+
+
+
+
+void main()
+{
+	//printf("We Ball\n");
+	printf("***Bianry Search***\n");
+
+	int arr[10];
+	int end = sizeof(arr / arr[0]);
+	int mid = end/2;
+	arr[10] = {0,1,2,3,4,5,6,7,8,9};
+	int start = 0;
+
+
+	while (keepGoing == 1)
+	{
+		if (arr[mid] == value){printf(" value found at index %i", mid);}
+		else if (arr[mid] != value)
+		{
+			if (arr[mid] > end){
+
+}
